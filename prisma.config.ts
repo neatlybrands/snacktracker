b7ef45e4abbc5c7da5adc30f3f21536caec1d9f0
Jing <neatlybrands@gmail.com>
2025-11-24 15:32:59 -0500

    feat: working SnackTracker setup with Prisma + Next.js

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+// prisma.config.ts
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    // Use the DATABASE_URL from .env
+    url: env("DATABASE_URL"),
+  },
+});
